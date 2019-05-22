4c831d458f2ab350a1d1bd9c70083aa632461a62
josephlbaker <joelucasbaker@gmail.com>
2019-05-21 17:18:39 -0700

    login working

--- piktogram-fe/src/components/SignedOut.js
+++ piktogram-fe/src/components/SignedOut.js
@@ -4,7 +4,7 @@ class SignedOut extends Component {
     render() {
         return (
             <div>
-                <a href="https://api.instagram.com/oauth/authorize/?client_id=9acb7ddc5f4d47178844c1faf0907acd&redirect_uri=https://localhost:3001/oauth/redirect&response_type=code">
+                <a href="https://api.instagram.com/oauth/authorize/?client_id=9acb7ddc5f4d47178844c1faf0907acd&redirect_uri=http://localhost:3000/redirect/&response_type=code">
                     Login with instagram
                 </a>
             </div>
