8a4998a5dda6379ed9759228febee42c4bd0a8c6
karma <karmadrukpa11@gmail.com>
2019-06-03 15:48:21 -0700

    made profile thumbnail grid

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import '../styles/Profile.css';
 
 export default class Profile extends Component {
 	state = {
@@ -49,9 +50,13 @@ export default class Profile extends Component {
 			let image = `http://localhost:3001/resources/images/${post.fileName}`;
 
 			return (
-				<div key={index}>
-					<h3>{post.description}</h3>
-					<img src={image} alt='' />
+				<div className='col-md-4' key={index}>
+					{/* <Col md={4} key={index}> */}
+					{/* <h3>{post.description}</h3> */}
+					<img src={image} alt='' className='w-100' />
+					{/* </Col> */}
+					{/* <h3>{post.description}</h3>
+					<img src={image} alt='' /> */}
 				</div>
 			);
 		} else {
@@ -69,10 +74,50 @@ export default class Profile extends Component {
 
 		if (this.state.user) {
 			return (
-				<div>
-					<h2>Posts</h2>
+				<div className='mw-custom'>
+					{/* <h2>Posts</h2> */}
+					<Container className='mt-5'>
+						<Row>
+							<Col md={3}>[[profile picture]]</Col>
+							<Col md={9}>
+								<h1> BIO</h1>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Deserunt voluptatibus nihil
+								animi est atque delectus consequatur dolore
+								excepturi fugit adipisci?
+							</Col>
+						</Row>
+					</Container>
+
+					<Container>
+						<Row>
+							<Col md={2}>[[storyimage]]</Col>
+							<Col md={2}>[[storyimage]]</Col>
+						</Row>
+					</Container>
+
+					<Container>
+						<Row>
+							{/* <Col md={4}></Col> */}
+							{posts ? (
+								posts.map(this._renderPosts)
+							) : (
+								'No posts yet...'
+							)}
+						</Row>
+					</Container>
+
+					<Container>
+						<Row>
+							<Col md={12}>
+								Footer. Lorem ipsum dolor sit, amet consectetur
+								adipisicing elit. Perferendis, molestias.
+							</Col>
+						</Row>
+					</Container>
+
+					{/* {posts ? posts.map(this._renderPosts) : 'No posts yet...'} */}
 					<Button onClick={this.handleSignOut}>Logout</Button>
-					{posts ? posts.map(this._renderPosts) : 'No posts yet...'}
 				</div>
 			);
 		} else {
