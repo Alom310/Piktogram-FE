548f26d88a044416c4e482a5e570c5890f79aa20
karma <karmadrukpa11@gmail.com>
2019-06-04 13:17:34 -0700

    styled newpost and searchbar

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,56 +1,64 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import '../styles/NewPost.css';
 
 export default class NewPost extends Component {
+	state = {
+		fileName    : null,
+		description : null
+	};
 
-  state = {
-    fileName: null,
-    description: null
-  }
+	handleChange = e => {
+		console.log(e.target.name);
+		if (e.target.name === 'image') {
+			this.setState({
+				fileName : e.target.files[0]
+			});
+		} else {
+			this.setState({
+				[e.target.name]: e.target.value
+			});
+		}
+	};
 
-  handleChange = (e) => {
-    console.log(e.target.name);
-    if (e.target.name === "image") {
-      this.setState({
-        fileName: e.target.files[0]
-      })
-    } else {
-      this.setState({
-        [e.target.name]: e.target.value
-      })
-    }
-  }
+	handleFormSubmit = () => {
+		if (localStorage.token) {
+			let formData = new FormData();
+			formData.append('description', this.state.description);
+			formData.append('image', this.state.fileName);
+			axios({
+				method  : 'POST',
+				url     : `http://localhost:3001/posts/createpost`,
+				headers : { token: localStorage.token },
+				data    : formData
+			})
+				.then(response => {
+					console.log(
+						'App successfully recieves a response',
+						response
+					);
+				})
+				.catch(err => console.log(err));
+		}
+	};
 
-  handleFormSubmit = () => {
-    if (localStorage.token) {
-      let formData = new FormData();
-      formData.append("description", this.state.description);
-      formData.append("image", this.state.fileName);
-      axios({
-        method: "POST",
-        url: `http://localhost:3001/posts/createpost`,
-        headers: { token: localStorage.token },
-        data: formData
-      })
-        .then(response => {
-          console.log('App successfully recieves a response', response)
-        })
-        .catch(err => console.log(err))
-    }
-  }
-
-  render() {
-    return (
-      <div>
-
-        <form onSubmit={this.handleFormSubmit}>
-          <input onChange={this.handleChange} type='file' name='image' />
-          <input onChange={this.handleChange} type='text' name='description' />
-          <input type='submit' value='Submit' />
-        </form>
-
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='new-post-form'>
+				<form onSubmit={this.handleFormSubmit}>
+					<input
+						onChange={this.handleChange}
+						type='file'
+						name='image'
+					/>
+					<input
+						onChange={this.handleChange}
+						type='text'
+						name='description'
+					/>
+					<input type='submit' value='Submit' />
+				</form>
+			</div>
+		);
+	}
 }
-
