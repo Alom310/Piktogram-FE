970bbaa9866e13084720504ea3c26016ea44bcf8
josephlbaker <joelucasbaker@gmail.com>
2019-06-13 15:02:56 -0700

    added heroku routes

--- src/urls/url-paths.js
+++ src/urls/url-paths.js
@@ -1,5 +1,5 @@
 let url = {};
-url.base=  "https://piktogram-be.herokuapp.com/";
+url.base = "https://cors-anywhere.herokuapp.com/https://immense-spire-50040.herokuapp.com/";
 // url.base = "http://localhost:3001/";
 url.login = url.base + "users/login";
 url.signup = url.base + "users/signup";
